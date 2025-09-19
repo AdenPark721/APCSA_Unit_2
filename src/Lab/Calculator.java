4782611dd37ebd232482a840db89c2c2517dd6a5
AdenPark721 <applelee2123@gmail.com>
2025-09-19 13:00:53 +0800

    Update Calculator.java

--- src/Lab/Calculator.java
+++ src/Lab/Calculator.java
@@ -40,7 +40,7 @@ public class Calculator {
     }
 
     public static double distance(int x1, int x2, int y1, int y2){
-        return Math.sqrt((x2 - x1) + (y2 - y1));
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
 }
 
